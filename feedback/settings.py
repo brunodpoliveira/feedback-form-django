e12bf58f5b34da1e3cadcaeaeefb34907891b875
Bruno Daniel <70975013+brunodpoliveira@users.noreply.github.com>
2021-02-19 16:01:36 -0300

    Update settings.py

--- feedback/settings.py
+++ feedback/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#$gx40nc2lk25^3o3@5e&4^kvdte06zv5mmj96adyw9fq2@w1m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
